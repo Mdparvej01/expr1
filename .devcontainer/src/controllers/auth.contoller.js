8d3503eb59f0985716afd57586a3740f15a84d45
Mdparvej01 <158189022+Mdparvej01@users.noreply.github.com>
2024-02-19 13:15:07 +0000

    user login and cookies

--- .devcontainer/src/controllers/auth.contoller.js
+++ .devcontainer/src/controllers/auth.contoller.js
@@ -4,6 +4,10 @@ import User from "../models/user.schema"
 import asyncHandler from "../service/asynchHandler";
 import CustomError from "../utils/Customerror";
 
+export const cookieOptions = {
+    expires:new Date(Date.now() + 3*24*60*60*1000), //ms
+    httpOnly:true //from only server side manages by server not user
+}
 
 // export const signUp=asyncHandler(aysnc(req,res) => {})
 
@@ -49,8 +53,19 @@ export const signUp=asyncHandler(async(req,res) => {
 
     const token = user.getJwttoken()
     //safety
-    user.password = undefined
+    user.password = undefined//vimp*** =>now user doesnt have password value..
+    //in theory we said that password has select false..
+    //why? we marked this as undefined=>bcz after when we make query for this ->user is restricted...
+    // now to send the response back to user
 
+    res.cookie("token",token,cookieOptions)   //i stored this token intothe users cookie =>Read client side and server side cookie 
+
+    res.status(200).json({
+        success:true,
+        token,
+        user, //bec we are sending this details backe.g=>===>>HEY USER "USERNAME"
+    })
+// what we learnt=> 1> wraped as try catch,how data is validated,whether user exist or not=>able to create new user=>safety flag =>sending responsce=>cookies..
     
 
 
--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import cors from "cors"
+import cookieparser from "cookie-parser"
 //creat obj from express
 const app = express()
-
-
+//  const {name,email,password} = req.body=>in this there are two portions one portion is hanled into rout and one in express app
+app.use(express.json())  //eccept the data in json formate..
+app.use(express.urlencoded({extended:true}))// this are compolsory settings toenable
+app.use(cors())
+app.use(cookieParser())
+//hw how CORES works......
+//we are excepting two types of data json data and urlencoded data=>now i want to access users cookie=>
+//this time in express json data coming in i am saying eccept data=>
+//some response will go from server to user also=>
+//by installing cookie parser=>server can access users cookie=>
+//use of it=>say we are sending response back to user byt before sending =>
+//i want to store this token in users cookie=>my server wants to access users cookie but by default it can not=>while sending him respons iwant to access user cookie
+//use--->res.cookie
 export default app;
