1d2ce5c46a3757becbc7e8cf1f010a77e9df523b
Mdparvej01 <158189022+Mdparvej01@users.noreply.github.com>
2024-03-05 15:25:03 +0000

    UploadedImageToS3

--- .devcontainer/src/config/index.js
+++ .devcontainer/src/config/index.js
@@ -8,7 +8,15 @@ const config = {
     MONGODB_URL:process.env.MONGODB_URL || "mongodb://localhost:27017/pjbase", //either grab port from process.env.PORT ORR use 5000
     //now i can use the same code in my dev enviroment and my production enviroment as well...
     JWT_SECRET:process.env.JWT_SECRET || "yoursecret", //tokens are defined now
-    JWT_EXPIRY:process.env.JWT_EXPIRY || "30d" //tokens are defined now...
+    JWT_EXPIRY:process.env.JWT_EXPIRY || "30d", //tokens are defined now...
+
+    // adding aws 
+    S3_ACCESS_KEY:process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY:process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME:process.env.S3_BUCKET_NAME,
+    S3_REGION:process.env.S3_REGION,
+    
+    
     
 }
 //u can import this config wherever u wamt...
